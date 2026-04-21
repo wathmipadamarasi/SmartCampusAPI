ebbbd330eae3c110ab6addeb4b399f3362ef0279
Wathmi Kodippili <wathmipadmarasi@gmail.com>
2026-04-21 15:56:31 +0530

    Update RoomNotEmptyExceptionMapper.java

--- src/main/java/com/smartcampus/exception/RoomNotEmptyExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/RoomNotEmptyExceptionMapper.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smartcampus.exception;
 
-/**
- *
- * @author WATHMI
- */
-public class RoomNotEmptyExceptionMapper {
-    
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+
+ //Part 5.1 - Maps RoomNotEmptyException to HTTP 409 Conflict.
+ 
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+
+    @Override
+    public Response toResponse(RoomNotEmptyException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", 409);
+        body.put("error", "Conflict");
+        body.put("message", ex.getMessage());
+        body.put("hint", "Remove all sensors from the room before attempting deletion.");
+
+        return Response.status(Response.Status.CONFLICT)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(body)
+                .build();
+    }
 }
