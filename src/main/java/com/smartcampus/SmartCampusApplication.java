fa1ff2238b7ae0f1a208cf5a3c1b660a54a95509
Wathmi Kodippili <wathmipadmarasi@gmail.com>
2026-04-21 15:47:09 +0530

    Update SmartCampusApplication.java

--- src/main/java/com/smartcampus/SmartCampusApplication.java
+++ src/main/java/com/smartcampus/SmartCampusApplication.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.smartcampus;
 
-/**
- *
- * @author WATHMI
- */
-public class SmartCampusApplication {
-    
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.smartcampus.resource.DiscoveryResource;
+import com.smartcampus.resource.RoomResource;
+import com.smartcampus.resource.SensorResource;
+import com.smartcampus.exception.RoomNotEmptyExceptionMapper;
+import com.smartcampus.exception.LinkedResourceNotFoundExceptionMapper;
+import com.smartcampus.exception.SensorUnavailableExceptionMapper;
+import com.smartcampus.exception.GlobalExceptionMapper;
+import com.smartcampus.filter.LoggingFilter;
+
+ //Part 1 -Task 1: JAX-RS Application Configuration
+ 
+ //@ApplicationPath sets the base URI for all resources to /api/v1
+ //This class registers all resource classes, exception mappers, and filters.
+ 
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        // Resources
+        classes.add(DiscoveryResource.class);
+        classes.add(RoomResource.class);
+        classes.add(SensorResource.class);
+
+        // Exception Mappers (Part 5)
+        classes.add(RoomNotEmptyExceptionMapper.class);
+        classes.add(LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(SensorUnavailableExceptionMapper.class);
+        classes.add(GlobalExceptionMapper.class);
+
+        // Filters (Part 5)
+        classes.add(LoggingFilter.class);
+
+        return classes;
+    }
 }
